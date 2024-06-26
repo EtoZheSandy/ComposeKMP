9c6275a5e1655086ac8210a5647e0430f6d9288f
Alexander <82713915+EtoZheSandy@users.noreply.github.com>
2024-06-26 17:27:09 +0300

    Мультиязычность

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,20 +1,21 @@
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
+import androidx.compose.ui.unit.dp
 import composekmp.composeapp.generated.resources.Res
-import composekmp.composeapp.generated.resources.compose_multiplatform
+import composekmp.composeapp.generated.resources.battery
+import composekmp.composeapp.generated.resources.kmp_logo
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
@@ -22,11 +23,13 @@ fun App(
     batteryManager: BatteryManager
 ) {
     MaterialTheme {
-        Box(
+        Column(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Заряд батареи ${batteryManager.getBatteryLevel()}")
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(text = "${stringResource(Res.string.battery)}: ${batteryManager.getBatteryLevel()}")
+            Image(painterResource(Res.drawable.kmp_logo), null)
         }
 
 //        var showContent by remember { mutableStateOf(false) }
